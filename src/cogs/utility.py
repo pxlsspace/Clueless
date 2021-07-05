0bb452fb7d422bc8118afe36b5ca41396113e3c7
GrayTurtles <grayturtles01@gmail.com>
2021-07-06 00:57:12 +0200

    move utility commands from main.py to cogs/utility.py

---
+++ src/cogs/utility.py
@@ -0,0 +1,83 @@
+import discord
+from discord.ext import commands
+import requests
+import json
+import os
+from dotenv import load_dotenv
+from cogs.utils.database import *
+
+class Utility(commands.Cog):
+    ''' Various utility commands'''
+
+    def __init__(self, client):
+        load_dotenv()
+        self.client = client
+
+    @commands.command(description="`>ping`: pong!")
+    async def ping(self,ctx):
+        await ctx.send(f"pong! (bot latency: `{round(self.client.latency*1000,2)}` ms)")
+
+
+    @commands.command(
+        usage = " <text>",
+        description = "Repeats your text."
+    )
+    async def echo(self,ctx,text):
+        await ctx.send(text)
+
+
+    @commands.command(
+        usage = " [prefix]",
+        description = "Changes the bot prefix for this server or shows the server prefix."
+    )
+    async def prefix(self,ctx,prefix=None):
+        if prefix == None:
+            prefix = ctx.prefix
+            await ctx.send("Current prefix: `"+prefix+"`")
+        else:
+            update_prefix(prefix,ctx.guild.id)
+            await ctx.send("Prefix set to `"+prefix+"`")
+
+
+    @commands.command(
+        usage = " <amount> <currency>",
+        description = "Converts currency from euro to rand or rand to euro."
+    )
+    async def convert(self,ctx,*args):
+        apikey = os.environ.get("CURRCONV_API_KEY")
+        r = requests.get(f'https://free.currconv.com/api/v7/convert?q=EUR_ZAR,ZAR_EUR&compact=ultra&apiKey={apikey}')
+        EUR_ZAR = json.loads(r.text)["EUR_ZAR"]
+        ZAR_EUR = json.loads(r.text)["ZAR_EUR"]
+
+        test_list_eur = ["euros","euro","eu","€","e"]
+        test_list_zar = ["rand","zar","r"]
+        currency=args[1]
+        currency=currency.lower()
+        res_eur = [ele for ele in test_list_eur if(ele in currency)]
+        res_zar = [ele for ele in test_list_zar if(ele in currency)]
+
+        amount = int(args[0])
+        if bool(res_eur):
+            await ctx.send(f'{amount}€ = {round(amount*EUR_ZAR,2)} rand')
+            return
+
+        if bool(res_zar):
+            
+            await ctx.send(f'{amount} rand = {round(amount*ZAR_EUR,2)}€')
+            return
+
+
+    @commands.command(aliases = ["rl"], hidden=True)
+    @commands.has_permissions(administrator=True)
+    async def reload(self,ctx,extension):
+        try:
+            self.client.reload_extension("cogs."+extension)
+        except Exception as e:
+            return await ctx.send('``` {}: {} ```'.format(type(e).__name__, e))
+            
+        await ctx.send(f"✅ Extension `{extension}` has been reloaded")
+
+
+
+def setup(client):
+    client.add_cog(Utility(client))
--- src/main.py
+++ src/main.py
@@ -50,68 +50,6 @@ async def on_guild_join(guild):
     create_server(guild.id,DEFAULT_PREFIX)
     print("joined a new server id: "+str(guild.id))
 
-### commands ###
-@client.command(description="`>ping`: pong!")
-async def ping(ctx):
-    await ctx.send(f"pong! (bot latency: `{round(client.latency*1000,2)}` ms)")
-
-@client.command(
-    usage = " <text>",
-    description = "Repeats your text."
-)
-async def echo(ctx,text):
-    await ctx.send(text)
-
-@client.command(
-    usage = " [prefix]",
-    description = "Changes the bot prefix for this server or shows the server prefix."
-)
-async def prefix(ctx,prefix=None):
-    if prefix == None:
-        prefix = ctx.prefix
-        await ctx.send("Current prefix: `"+prefix+"`")
-    else:
-        update_prefix(prefix,ctx.guild.id)
-        await ctx.send("Prefix set to `"+prefix+"`")
-
-@client.command(
-    usage = " <amount> <currency>",
-    description = "Converts currency from euro to rand or rand to euro."
-)
-async def convert(ctx,*args):
-    apikey = os.environ.get("CURRCONV_API_KEY")
-    r = requests.get(f'https://free.currconv.com/api/v7/convert?q=EUR_ZAR,ZAR_EUR&compact=ultra&apiKey={apikey}')
-    EUR_ZAR = json.loads(r.text)["EUR_ZAR"]
-    ZAR_EUR = json.loads(r.text)["ZAR_EUR"]
-
-    test_list_eur = ["euros","euro","eu","€","e"]
-    test_list_zar = ["rand","zar","r"]
-    currency=args[1]
-    currency=currency.lower()
-    res_eur = [ele for ele in test_list_eur if(ele in currency)]
-    res_zar = [ele for ele in test_list_zar if(ele in currency)]
-
-    amount = int(args[0])
-    if bool(res_eur):
-        await ctx.send(f'{amount}€ = {round(amount*EUR_ZAR,2)} rand')
-        return
-
-    if bool(res_zar):
-        
-        await ctx.send(f'{amount} rand = {round(amount*ZAR_EUR,2)}€')
-        return
-
-
-@client.command(aliases = ["rl"])
-@commands.has_permissions(administrator=True)
-async def reload(ctx,extension):
-    try:
-        client.reload_extension("cogs."+extension)
-    except Exception as e:
-        return await ctx.send('``` {}: {} ```'.format(type(e).__name__, e))
-        
-    await ctx.send(f"✅ Extension `{extension}` has been reloaded")
-
 if __name__ == "__main__":
     # loading cogs
     for extension in os.listdir("./src/cogs"):

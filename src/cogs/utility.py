3b5ed003144f09438952f7137325da5943076edf
GrayTurtles <grayturtles01@gmail.com>
2021-07-13 21:09:38 +0200

    check on arguments in the 'convert' command

--- src/cogs/utility.py
+++ src/cogs/utility.py
@@ -14,22 +14,22 @@ class Utility(commands.Cog):
         load_dotenv()
         self.client = client
 
-    @commands.command(description="`>ping`: pong!")
+    @commands.command(description="pong! (show the bot latency)")
     async def ping(self,ctx):
         await ctx.send(f"pong! (bot latency: `{round(self.client.latency*1000,2)}` ms)")
 
 
     @commands.command(
-        usage = " <text>",
-        description = "Repeats your text."
+        usage = "<text>",
+        description = "Repeat your text."
     )
     async def echo(self,ctx,text):
         await ctx.send(text)
 
 
     @commands.command(
-        usage = " [prefix]",
-        description = "Changes the bot prefix for this server or shows the server prefix."
+        usage = "[prefix]",
+        description = "Change or display the bot prefix."
     )
     async def prefix(self,ctx,prefix=None):
         if prefix == None:
@@ -41,39 +41,37 @@ class Utility(commands.Cog):
 
 
     @commands.command(
-        usage = " <amount> <currency>",
-        description = "Converts currency from euro to rand or rand to euro."
+        usage = "<amount> <currency>",
+        description = "Convert currency between Euro and South African Rand.",
+        help = """- `<amount>`: a number to convert
+                  - `<currency>`: either: 'euros', 'euro', 'EUR', '€', 'e' or 'rand', 'ZAR', 'R' (not case-sensitive)"""
     )
-    async def convert(self,ctx,*args):
+    async def convert(self,ctx,amount,currency):
+        try:
+            amount = float(amount)
+        except ValueError:
+            return await ctx.send("❌ `amount` parameter must be a number.")
         apikey = os.environ.get("CURRCONV_API_KEY")
         r = requests.get(f'https://free.currconv.com/api/v7/convert?q=EUR_ZAR,ZAR_EUR&compact=ultra&apiKey={apikey}')
         EUR_ZAR = json.loads(r.text)["EUR_ZAR"]
         ZAR_EUR = json.loads(r.text)["ZAR_EUR"]
 
-        test_list_eur = ["euros","euro","eu","€","e"]
+        test_list_eur = ["euros","euro","eu","eur","€","e"]
         test_list_zar = ["rand","zar","r"]
-        currency=args[1]
-        currency=currency.lower()
-        res_eur = [ele for ele in test_list_eur if(ele in currency)]
-        res_zar = [ele for ele in test_list_zar if(ele in currency)]
 
-        amount = int(args[0])
-        if bool(res_eur):
-            await ctx.send(f'{amount}€ = {round(amount*EUR_ZAR,2)} rand')
-            return
-
-        if bool(res_zar):
-            
-            await ctx.send(f'{amount} rand = {round(amount*ZAR_EUR,2)}€')
-            return
-
+        if currency.lower() in test_list_eur:
+            return await ctx.send(f'{amount}€ = {round(amount*EUR_ZAR,2)} rand')
+        elif currency.lower() in test_list_zar:
+            return await ctx.send(f'{amount} rand = {round(amount*ZAR_EUR,2)}€')
+        else:
+            return await ctx.send("❌ Invalid `currency` parameter.")
 
     @commands.command(
-        usage=" <?d?h?m?s>",
+        usage="<?d?h?m?s>",
         description = "Converts time formats.",
-        aliases = ["timeconvert","tconvert","time"]
+        aliases = ["converttime","tconvert","tc"]
         )
-    async def converttime(self,ctx,input):
+    async def timeconvert(self,ctx,input):
         time = str_to_td(input)
         if not time:
             return await ctx.send(f"❌ Invalid `time` parameter, format must be `{ctx.prefix}{ctx.command.name}{ctx.command.usage}`.")

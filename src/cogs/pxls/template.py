8bffb00250c43df598104b5ebd11ca111845b617
GrayTurtles <grayturtles01@gmail.com>
2021-11-17 17:08:21 +0100

    Add a template making command

---
+++ src/cogs/pxls/template.py
@@ -0,0 +1,154 @@
+import discord
+import numpy as np
+import urllib.parse
+import time
+from PIL import Image
+from io import BytesIO
+from discord.ext import commands
+from discord_slash import cog_ext, SlashContext
+from discord_slash.utils.manage_commands import create_option, create_choice
+
+from utils.arguments_parser import MyParser
+from utils.discord_utils import format_number, get_image_from_message, image_to_file
+from utils.pxls.template import *
+from utils.setup import stats, GUILD_IDS
+
+class Template(commands.Cog):
+
+    def __init__(self,client) -> None:
+        self.client = client
+
+    @cog_ext.cog_slash(
+        name="template",
+        description="Generate a template link from an image.",
+        guild_ids=GUILD_IDS,
+        options=[
+        create_option(
+            name="image",
+            description="The URL of the image you want to templatize.",
+            option_type=3,
+            required=False
+        ),
+        create_option(
+            name="style",
+            description="The template style you want. (default: custom)",
+            option_type=3,
+            required=False,
+            choices= [create_choice(name=s['name'],value=s['name']) for s in STYLES]
+        ),
+        create_option(
+            name="glow",
+            description="To add glow to the template. (default: False)",
+            option_type=5,
+            required=False
+        ),
+        create_option(
+            name="title",
+            description="The template title.",
+            option_type=3,
+            required=False
+        )]
+    )
+    async def _highlight(self,ctx:SlashContext, image=None, style=None, glow=None, title=None):
+        await ctx.defer()
+        await self.template(ctx,image,style,glow,title)
+
+    @commands.command(
+        name="template",
+        description="Generate a template link from an image.",
+        usage = "<image|url> [-style <style>] [-glow] [-title <title>]",
+        help="""- `<image|url>`: an image URL or an attached file
+              - `[-style <style>]`: the template style you want (use `>styles` to see the list)
+              - `[-glow]`: add glow to the template
+              - `[-title <title>]`: the template title""",
+        aliases=["templatize","temp"])
+    async def p_template(self,ctx,*args):
+
+        parser = MyParser()
+        parser.add_argument("url",action="store",nargs="*")
+        parser.add_argument("-style",action="store",required=False)
+        parser.add_argument("-glow",action="store_true",default=False)
+        parser.add_argument("-title",action="store",required=False)
+
+        try:
+            parsed_args= parser.parse_args(args)
+        except ValueError as e:
+            return await ctx.send(f'❌ {e}')
+        url = parsed_args.url[0] if parsed_args.url else None
+        async with ctx.typing():
+            await self.template(ctx, url, parsed_args.style, parsed_args.glow, parsed_args.title)
+
+    async def template(self,ctx,image_url,style_name,glow,title):
+        # get the image from the message
+        try:
+            img, url = await get_image_from_message(ctx,image_url,accept_emojis=False)
+        except ValueError as e:
+            return await ctx.send(f"❌ {e}")
+        img = Image.open(BytesIO(img))
+        img = img.convert('RGBA')
+
+        start = time.time()
+        # check on the style
+        if not style_name:
+            style_name = 'custom' # default style
+        style = get_style(style_name)
+        if not style:
+            styles_available="**Available Styles:**\n"
+            for s in STYLES:
+                styles_available += ("\t• {0} ({1}x{1})\n").format(s["name"],s["size"])
+            return await ctx.send(f"❌ Unknown style '{style_name}'.\n{styles_available}")
+
+        # check on the glow
+        if glow:
+            glow_opacity = 0.2
+        else:
+            glow_opacity = 0
+
+        # convert the image to a template style
+        img_array = np.array(img)
+        template_array = await self.client.loop.run_in_executor(None,templatize,style,img_array, glow_opacity)
+        template_image = Image.fromarray(template_array)
+        alpha_values = img_array[:,:,3]
+        total_amount = np.sum(alpha_values==255)
+        total_amount = format_number(int(total_amount))
+        end = time.time()
+
+        # create and send the image
+        embed = discord.Embed(title="**Template Image**",color=0x66c5cc)
+        embed.description = f"**Title**: {title if title else '`N/A`'}\n**Style**: {style_name}\n**Glow**: {'yes' if glow else 'no'}\n**Size**: {total_amount} pixels ({img.width}x{img.height})"
+        embed.set_footer(text="Warning: if you delete this message the template might break.")
+        embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
+        embed.set_thumbnail(url=url)
+        file_title = title.replace(" ","_") if title else "template"
+        file_title +=".png"
+        file = image_to_file(template_image,file_title)
+        m = await ctx.send(embed=embed,file=file)
+
+        # create a template link with the sent image
+        template_title = f'&title={urllib.parse.quote(title)}' if title else ''
+        template_image_url = m.attachments[0].url
+        try: # we're using a try/except block so we can still make templates if the board info is unreachable
+            x = int(stats.board_info["width"]/2)
+            y = int(stats.board_info["height"]/2)
+        except Exception:
+            x = y = 500
+        ox = x - img.width/2
+        oy = y - img.width/2
+        template_url = f"https://pxls.space/#x={x}&y={y}&scale=5&template={urllib.parse.quote(template_image_url)}&ox={ox}&oy={oy}&tw={img.width}&oo=1{template_title}"
+        template_embed = discord.Embed(title="**Template Link**",description=template_url, color=0x66c5cc)
+        template_embed.set_footer(text=f"Generated in {round((end-start),3)}s")
+        await ctx.send(embed=template_embed)
+
+    @commands.command(
+        name="styles",
+        description="List the template styles available.",
+        aliases = ["style"])
+    async def styles(self,ctx):
+        styles_available="**Available Styles:**\n"
+        for s in STYLES:
+            styles_available += ("\t• {0} ({1}x{1})\n").format(s["name"],s["size"])
+        return await ctx.send(styles_available)
+
+def setup(client):
+    client.add_cog(Template(client))
+    
---
+++ src/utils/pxls/template.py
@@ -0,0 +1,150 @@
+# code base by Nanineye#2417
+
+import os
+import numpy as np
+from PIL import Image
+from utils.setup import stats
+from utils.image.image_utils import hex_to_rgb
+
+class InvalidStyleException(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+def get_style_from_image(style_name):
+    try:
+        basepath = os.path.dirname(__file__)
+        styles_folder = os.path.abspath(os.path.join(basepath, "..", "..","..",
+        "ressources","styles"))
+        filename = os.path.join(styles_folder,style_name+".png")
+        img = Image.open(filename)
+    except FileNotFoundError:
+        raise InvalidStyleException(f"Can't find {filename}")
+
+    try:
+        img_array = np.array(img)
+        mask = img_array[:,:,3] != 0
+        symbols_per_line = 16
+        style_size = int(img.width/symbols_per_line)
+        res = np.zeros((symbols_per_line*symbols_per_line,style_size,style_size))
+        color_idx = 0
+        for i in range(symbols_per_line):
+            for j in range(symbols_per_line):
+                x0 = i*style_size
+                y0 = j*style_size
+                x1 = x0 + style_size
+                y1 = y0 + style_size
+                symbol = mask[x0:x1,y0:y1]
+                res[color_idx] = symbol
+                color_idx+=1
+        style = {
+            'name': style_name,
+            'size': style_size,
+            'array': res
+        }
+        return style
+
+    except Exception as e:
+        raise InvalidStyleException(f"Unexpected error: {e}")
+
+none = {
+    'name': 'none',
+    'size': 1,
+    'array': np.array([[[1]]]*255)
+}
+
+dotted = {
+    'name':'dotted',
+    'size':3,
+    'array': np.array([[[0,0,0],[0,1,0],[0,0,0]]]*255)
+}
+
+plus = {
+    'name':'plus',
+    'size':3,
+    'array': np.array([[[0,1,0],[1,1,1],[0,1,0]]]*255)
+}
+
+bigdotted = {
+    'name':'bigdotted',
+    'size':5,
+    'array': np.array([[
+        [0,0,0,0,0],
+        [0,1,1,1,0],
+        [0,1,1,1,0],
+        [0,1,1,1,0],
+        [0,0,0,0,0],
+    ]]*255)
+}
+
+STYLES = [none, dotted, plus, bigdotted]
+custom_styles = ["custom", "pgcustom", "numbers"]
+
+for s in custom_styles:
+    try:
+        STYLES.append(get_style_from_image(s))
+    except InvalidStyleException as e:
+        print(f"Warning: failed to load '{s}' style: {e}")
+
+
+def get_style(style_name:str):
+    for style in STYLES:
+       if style['name'] == style_name:
+           return style
+    return None
+
+def get_rgba_palette():
+    palette = stats.get_palette()
+    palette = np.array([c['value'] for c in palette])
+    res=[]
+    for i in palette:
+        c=hex_to_rgb(i,"RGBA")
+        res.append(c)
+    return(np.array(res))
+
+def stylize(style,stylesize,palette,glow_opacity=0):
+    res = np.zeros((len(palette),stylesize,stylesize,4))
+    for i in range(len(palette)):
+        cstyle = np.zeros((stylesize,stylesize,4))
+        glow_value = np.copy(palette[i])
+        glow_value[3] = glow_opacity*255
+        cstyle[:,:] = glow_value
+        for j in range(stylesize):
+            for k in range(stylesize):
+                if style[i,j,k]:
+                    cstyle[j,k] = palette[i]*style[i,j,k]
+        res[i] = cstyle
+    return(res)
+
+def reduce(rendered_array,palette):
+    best_match_idx = np.zeros(rendered_array.shape[:2], dtype=np.uint8)
+    best_match_dist = np.full(rendered_array.shape[:2], 500)  # 500<sqrt(3*255^2)
+
+    for idx, color in enumerate(palette):
+        color_distance = np.linalg.norm(rendered_array - color, axis=-1)
+        closer_mask = color_distance < best_match_dist
+        best_match_dist[closer_mask] = color_distance[closer_mask]
+        best_match_idx[closer_mask] = idx
+
+    alpha_values = rendered_array[:,:,3]
+    best_match_idx[alpha_values!=255] = 255 # alpha index
+
+    return best_match_idx
+
+def templatize(style:dict, image:Image.Image, glow_opacity=0) -> np.ndarray:
+    style_array = style["array"]
+    style_size = style["size"]
+    image_array = np.array(image)
+
+    n = image_array.shape[0]
+    m = image_array.shape[1]
+
+    palette = get_rgba_palette()
+    red = reduce(image_array,palette)
+    st = stylize(style_array,style_size,palette,glow_opacity)
+    res = np.zeros((style_size*n,style_size*m,4),dtype=np.uint8)
+    for i in range(n):
+        for j in range(m):
+            if red[i,j] != 255: # non-alpha values
+                res[style_size*i:style_size*i+style_size,style_size*j:style_size*j+style_size] = st[red[i][j]]
+    res = np.array(res)
+    return res

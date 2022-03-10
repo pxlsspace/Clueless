7dbd816d62e2d0304ff28956142549edbc415718
GrayTurtles <grayturtles01@gmail.com>
2022-03-10 16:43:16 +0100

    Fix error when joining/leaving a server without icon

--- src/main.py
+++ src/main.py
@@ -333,7 +333,8 @@ async def on_guild_join(guild):
     embed.add_field(name="**Server Name**", value=guild.name)
     embed.add_field(name="**Owner**", value=guild.owner)
     embed.add_field(name="**Members**", value=guild.member_count)
-    embed.set_thumbnail(url=guild.icon.url)
+    if guild.icon:
+        embed.set_thumbnail(url=guild.icon.url)
     embed.set_footer(text=f"ID • {guild.id} | Server Created")
     await log_channel.send(embed=embed)
 
@@ -360,7 +361,8 @@ async def on_guild_remove(guild):
     embed.add_field(name="**Server Name**", value=guild.name)
     embed.add_field(name="**Owner**", value=guild.owner)
     embed.add_field(name="**Members**", value=guild.member_count)
-    embed.set_thumbnail(url=guild.icon.url)
+    if guild.icon:
+        embed.set_thumbnail(url=guild.icon.url)
     embed.set_footer(text=f"ID • {guild.id} | Server Created")
     await log_channel.send(embed=embed)
 

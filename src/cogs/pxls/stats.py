7bf9b0cfa4f41f808e56ff470610899cc46cbbe7
Ellum-Universe <152664831+Ellum-Universe@users.noreply.github.com>
2025-09-18 14:19:24 -0400

    added perfect_amount

--- src/cogs/pxls/stats.py
+++ src/cogs/pxls/stats.py
@@ -358,13 +358,14 @@ class PxlsStats(commands.Cog):
             dt2 = user_row["datetime"]
             dt1 = record_list[0]["datetime"]
             best_possible, average_cooldown = await get_best_possible(dt1, dt2)
+            perfect_amount = int(best_possible * 0.995)
             fast_amount = int(best_possible * 0.95)
 
             if last_15m > best_possible:
                 status = "online (botting)"
                 status_emoji = STATUS_EMOJIS["bot"]
                 embed_color = 0x7CE1EC
-             elif last_15m = best_possible:
+             elif last_15m >= perfect_amount:
                 status = "online (perfect)"
                 status_emoji = STATUS_EMOJIS["perfect"]
                 embed_color = 0xFFA4D0
